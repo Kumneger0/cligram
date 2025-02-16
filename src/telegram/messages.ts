36f1dfe820e7a9c702c4cf929c79488d9b62d56a
kumneger <kumnegerwondimu01@gmail.com>
2025-02-16 21:38:39 +0300

    remove blessed use ink instead

--- src/telegram/messages.ts
+++ src/telegram/messages.ts
@@ -1,66 +1,70 @@
-import { TelegramClient, Api } from 'telegram';
-import { MessagesSlice, User } from '../lib/types/index.js';
-import { ChatUser, FormattedMessage, MessageMedia, eventClassNames } from '../types.js';
-import { getUserChats, getUserInfo } from './client.js';
-import { rerenderSidebar, selectedName } from '../ui/sidebar.js';
-import { getTelegramClient } from '../lib/utils/auth.js';
-import { downloadMedia } from '@/lib/utils/handleMedia.js';
+import { Api, TelegramClient } from 'telegram';
+import { MessagesSlice, User } from '../lib/types/index';
+import { getTelegramClient } from '../lib/utils/auth';
+import { ChatUser, FormattedMessage, eventClassNames } from '../types';
+import { getUserChats, getUserInfo } from './client';
 
 export async function getConversationHistory(
-    { accessHash, firstName, peerId: userId }: ChatUser,
-    limit: number = 100
+	{ accessHash, firstName, peerId: userId }: ChatUser,
+	limit: number = 100
 ): Promise<FormattedMessage[]> {
-    const client = await getTelegramClient();
-    if (!client.connected) await client.connect();
+	const client = await getTelegramClient();
+	if (!client.connected) await client.connect();
 
-    const result = (await client.invoke(
-        new Api.messages.GetHistory({
-            peer: new Api.InputPeerUser({
-                userId: userId as unknown as bigInt.BigInteger,
-                accessHash
-            }),
-            limit
-        })
-    )) as unknown as MessagesSlice;
+	const result = (await client.invoke(
+		new Api.messages.GetHistory({
+			peer: new Api.InputPeerUser({
+				userId: userId as unknown as bigInt.BigInteger,
+				accessHash
+			}),
+			limit
+		})
+	)) as unknown as MessagesSlice;
 
-    return await Promise.all(result.messages.reverse().map(
-        async (message): Promise<FormattedMessage> => {
-            // const media = message.media as MessageMedia
+	return await Promise.all(
+		result.messages.reverse().map(async (message): Promise<FormattedMessage> => {
+			// const media = message.media as MessageMedia
 
-            return ({
-                sender: message.out ? 'you' : firstName,
-                content: message.message,
-                isFromMe: message.out,
-                //TODO: implement media download later after updatin the layout
-                media: null
-                // media: media && media.className == 'MessageMediaPhoto' ? await downloadMedia({ media, size: 'large' }) : null,
-            })
-        }
-    ))
+			return {
+				id: message.id,
+				sender: message.out ? 'you' : firstName,
+				content: message.message,
+				isFromMe: message.out,
+				//TODO: implement media download later after updatin the layout
+				media: null
+				// media: media && media.className == 'MessageMediaPhoto' ? await downloadMedia({ media, size: 'large' }) : null,
+			};
+		})
+	);
 }
-export const listenForUserMessages = async (client: TelegramClient) => {
-    if (!client.connected) await client.connect();
-    console.log('Listening for messages');
+export const listenForUserMessages = async (
+	client: TelegramClient,
+	setUserChats: React.Dispatch<React.SetStateAction<ChatUser[]>>,
+	selectedName: string
+) => {
+	if (!client.connected) await client.connect();
+	console.log('Listening for messages');
 
-    client.addEventHandler(async (event) => {
-        const userId = event.userId;
-        if (userId) {
-            const user = (await getUserInfo(client, userId)) as unknown as User;
+	client.addEventHandler(async (event) => {
+		const userId = event.userId;
+		if (userId) {
+			const user = (await getUserInfo(client, userId)) as unknown as User;
 
-            const isNewMessage =
-                (event.className as (typeof eventClassNames)[number]) === 'UpdateShortMessage';
+			const isNewMessage =
+				(event.className as (typeof eventClassNames)[number]) === 'UpdateShortMessage';
 
-            if (isNewMessage) {
-                if (user.firstName !== selectedName) {
-                    const users = await getUserChats(client);
-                    const userChats = users
-                        .filter(({ isBot }) => !isBot)
-                        .map(({ firstName }) => firstName)
-                        .map((name) => (name === selectedName ? name + ' *' : name));
-
-                    rerenderSidebar(userChats);
-                }
-            }
-        }
-    });
+			if (isNewMessage) {
+				if (user.firstName !== selectedName) {
+					const users = await getUserChats(client);
+					const userChats = users
+						.filter(({ isBot }) => !isBot)
+						.map(({ firstName, ...rest }) => ({
+							firstName: firstName === selectedName ? firstName + ' *' : firstName,
+							...rest
+						}));
+					setUserChats(userChats);
+				}
+			}
+		}
+	});
 };
