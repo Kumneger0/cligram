5803fa37f4b25b68f2bc1c1d97fa15157da7e532
kumneger <kumnegerwondimu01@gmail.com>
2025-02-17 15:27:34 +0300

    feat:add pagination for retiving user conversation

--- src/telegram/messages.ts
+++ src/telegram/messages.ts
@@ -3,24 +3,65 @@ import { MessagesSlice, User } from '../lib/types/index';
 import { ChatUser, FormattedMessage, eventClassNames } from '../types';
 import { getUserChats, getUserInfo } from './client';
 
-
-
-export const sendMessage = async (client: TelegramClient, userToSend: { peerId: bigInt.BigInteger, accessHash: bigInt.BigInteger }, message: string) => {
-    if (!client.connected) await client.connect();
-    client.sendMessage(
-        new Api.InputPeerUser({
-            userId: userToSend?.peerId,
-            accessHash: userToSend?.accessHash
-        }),
-        {
-            message: message
-        }
-    );
+export const sendMessage = async (
+	client: TelegramClient,
+	userToSend: { peerId: bigInt.BigInteger; accessHash: bigInt.BigInteger },
+	message: string
+) => {
+	if (!client.connected) await client.connect();
+	client.sendMessage(
+		new Api.InputPeerUser({
+			userId: userToSend?.peerId,
+			accessHash: userToSend?.accessHash
+		}),
+		{
+			message: message
+		}
+	);
 };
 
+export async function getAllMessages(
+	client: TelegramClient,
+	{ accessHash, firstName, peerId: userId }: ChatUser,
+	offsetId?: number
+): Promise<FormattedMessage[]> {
+	if (!client.connected) await client.connect();
+	const messages = [];
+
+	for await (const message of client.iterMessages(
+		new Api.InputPeerUser({
+			userId: userId as unknown as bigInt.BigInteger,
+			accessHash
+		}),
+		{ limit: 20, offsetId }
+	)) {
+		messages.push(message);
+	}
+	const orgnizedMessages = (
+		await Promise.all(
+			messages.reverse()?.map(async (message): Promise<FormattedMessage> => {
+				// const media = message.media as MessageMedia
+
+				return {
+					id: message.id,
+					sender: message.out ? 'you' : firstName,
+					content: message.message,
+					isFromMe: !!message.out,
+					//TODO: implement media download later after updatin the layout
+					media: null
+					// media: media && media.className == 'MessageMediaPhoto' ? await downloadMedia({ media, size: 'large' }) : null,
+				};
+			})
+		)
+	)
+		?.map(({ content, ...rest }) => ({ content: content?.trim(), ...rest }))
+		?.filter((msg) => msg?.content?.length > 0);
+
+	return orgnizedMessages;
+}
 
 export async function getConversationHistory(
-    client: TelegramClient,
+	client: TelegramClient,
 	{ accessHash, firstName, peerId: userId }: ChatUser,
 	limit: number = 100
 ): Promise<FormattedMessage[]> {
