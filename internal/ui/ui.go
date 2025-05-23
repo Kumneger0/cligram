2097e76dca10f1caffbb94cb15f6b138a72ccb20
kumneger <kumnegerwondimu01@gmail.com>
2025-05-23 15:22:41 +0300

    add Group Mode with dummy fake data

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -21,10 +21,10 @@ type CustomDelegate struct {
 
 func (d CustomDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
 	var title string
-	
+
 	if entry, ok := item.(UserInfo); ok {
 		title = entry.Title()
-	} else if entry, ok := item.(ChannelInfo); ok {
+	} else if entry, ok := item.(ChannelAndGroupInfo); ok {
 		title = entry.Title()
 	} else {
 		return
@@ -69,7 +69,7 @@ func GetFakeChannels() []list.Item {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	var fakeChannels []ChannelInfo
+	var fakeChannels []ChannelAndGroupInfo
 	err = json.Unmarshal(testJson, &fakeChannels)
 
 	if err != nil {
@@ -97,34 +97,15 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "tab":
-			currentlyFoucsedOn := m.FocusedOn
-			if currentlyFoucsedOn == "sideBar" {
-				m.FocusedOn = "mainView"
-			} else if currentlyFoucsedOn == "mainView" {
-				m.FocusedOn = "input"
-			} else {
-				m.FocusedOn = "sideBar"
-			}
-			return m, nil
+			return changeFocusMode(m, "tab")
 		case "c":
-			if m.Mode == "users" {
-				m.Mode = "channels"
-			}
-			return m, nil
+			return changeSideBarMode(m, "c")
 		case "u":
-			if m.Mode == "channels" {
-				m.Mode = "users"
-			}
-			return m, nil
+			return changeSideBarMode(m, "u")
+		case "g":
+			return changeSideBarMode(m, "g")
 		case "enter":
-			if m.Mode == "users" {
-				m.SelectedUser = m.Users.SelectedItem().(UserInfo)
-			}
-			if m.Mode == "channels" {
-				m.SelectedChannel = m.Channels.SelectedItem().(ChannelInfo)
-			}
-			//TODO: kick off new rpc request to js backend to get the conversation for new chat
-			return m, nil
+			return handleUserChange(m)
 		}
 	case tea.WindowSizeMsg:
 		m.Width = msg.Width - 4
@@ -132,6 +113,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.updateViewport()
 	}
 
+	return updateFocusedComponent(m, msg)
+}
+
+func updateFocusedComponent(m Model, msg tea.Msg) (Model, tea.Cmd) {
 	var cmd tea.Cmd
 
 	if m.FocusedOn == "input" {
@@ -139,20 +124,60 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.Input, cmd = m.Input.Update(msg)
 	} else if m.FocusedOn == "sideBar" {
 		m.Input.Blur()
-        if m.Mode == "channels" {
-            m.Channels, cmd = m.Channels.Update(msg)
-        } else if m.Mode == "users"{
+		if m.Mode == "channels" {
+			m.Channels, cmd = m.Channels.Update(msg)
+		} else if m.Mode == "users" {
 			m.Users, cmd = m.Users.Update(msg)
 		} else {
-			//if we reach at this point we must be in group mode 
-			// as of wrting this im not handling the group mode 
-			// TODO: handle group mode
+			m.Groups, cmd = m.Groups.Update(msg)
 		}
 
 	} else {
 		m.Vp, cmd = m.Vp.Update(msg)
 	}
 	return m, cmd
+}
+
+func handleUserChange(m Model) (Model, tea.Cmd) {
+	if m.Mode == "users" {
+		m.SelectedUser = m.Users.SelectedItem().(UserInfo)
+	}
+	if m.Mode == "channels" {
+		m.SelectedChannel = m.Channels.SelectedItem().(ChannelAndGroupInfo)
+	}
+	if m.Mode == "groups" {
+		m.SelectedGroup = m.Groups.SelectedItem().(ChannelAndGroupInfo)
+	}
+
+	//TODO: kick off new rpc request to js backend to get the conversation for new chat
+	return m, nil
+}
+
+func changeFocusMode(m Model, msg string) (Model, tea.Cmd) {
+	currentlyFoucsedOn := m.FocusedOn
+	if currentlyFoucsedOn == "sideBar" {
+		m.FocusedOn = "mainView"
+	} else if currentlyFoucsedOn == "mainView" {
+		m.FocusedOn = "input"
+	} else {
+		m.FocusedOn = "sideBar"
+	}
+	return m, nil
+}
+
+func changeSideBarMode(m Model, msg string) (Model, tea.Cmd) {
+	switch msg {
+	case "c":
+		m.Mode = "channels"
+		return m, nil
+	case "u":
+		m.Mode = "users"
+		return m, nil
+	case "g":
+		m.Mode = "groups"
+		return m, nil
+	}
+	return m, nil
 }
 
 func (m *Model) updateViewport() {
@@ -198,6 +223,9 @@ func setItemStyles(m Model) string {
 	if m.Mode == "channels" {
 		userNameOrChannelName = m.SelectedChannel.FilterValue()
 	}
+	if m.Mode == "groups" {
+		userNameOrChannelName = m.SelectedGroup.FilterValue()
+	}
 
 	title := titleStyle.Render(userNameOrChannelName)
 	line := strings.Repeat("─", max(0, mainWidth-4-lipgloss.Width(title)))
@@ -210,14 +238,14 @@ func setItemStyles(m Model) string {
 	)
 
 	chatView := mainStyle.Render(mainContent)
-    
+
 	var sideBarContent string
 	if m.Mode == "users" {
 		sideBarContent = m.Users.View()
 	} else if m.Mode == "channels" {
 		sideBarContent = m.Channels.View()
 	} else {
-		sideBarContent = ""
+		sideBarContent = m.Groups.View()
 	}
 
 	sidebar := getSideBarStyles(sidebarWidth, contentHeight, m).Render(sideBarContent)
@@ -225,6 +253,7 @@ func setItemStyles(m Model) string {
 	if m.FocusedOn == "input" {
 		m.Input.Focus()
 	}
+
 	inputView := getInputStyle(m, inputHeight).Render(m.Input.View())
 	row := lipgloss.JoinHorizontal(lipgloss.Top, sidebar, chatView)
 	ui := lipgloss.JoinVertical(lipgloss.Top, row, inputView)
@@ -244,6 +273,9 @@ func (m Model) View() string {
 	m.Channels.Title = "Channels"
 	m.Channels.SetShowStatusBar(false)
 	m.Users.SetShowStatusBar(false)
+	m.Groups.Title = "Groups"
+	m.Groups.SetShowStatusBar(false)
+
 	ui := setItemStyles(m)
 	return ui
 }
