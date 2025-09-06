88598ee6ff109c0763d23f56c96fdf6d42b17d99
kumneger0 <kumnegerwondimu01@gmail.com>
2025-09-06 20:15:59 +0300

    fix(perf): use cache sync.once to cache app logo

--- internal/notification/notify.go
+++ internal/notification/notify.go
@@ -2,22 +2,35 @@ package notification
 
 import (
 	"log/slog"
+	"sync"
 
 	"github.com/gen2brain/beeep"
 	"github.com/kumneger0/cligram/assets"
 )
 
+var (
+	once sync.Once
+)
+
+func getAppLogo() *[]byte {
+	var appLogo *[]byte
+	once.Do(func() {
+		logo, err := assets.Assets.ReadFile("logo.png")
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+		appLogo = &logo
+	})
+	return appLogo
+}
+
 func Notify(title string, message string) {
 	beeep.AppName = "Cligram"
-	logo, err := assets.Assets.ReadFile("logo.png")
+	logo := getAppLogo
 
+	err := beeep.Alert(title, message, logo)
 	if err != nil {
 		slog.Error(err.Error())
-		panic(err)
-	}
-
-	err = beeep.Alert(title, message, logo)
-	if err != nil {
-		panic(err)
 	}
 }
